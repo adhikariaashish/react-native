3b23ba6d2141f2e5ba773f7e005b45aede9d4518
Aashish Adhikari <axisinfinity69@gmail.com>
2025-12-09 19:50:14 +0545

    update dependencies

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="flex-1 justify-center items-center">Welcome! </Text>
     </View>
   );
 }
