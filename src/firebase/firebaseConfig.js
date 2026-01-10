e601bdc14b94138fd4ddd214ad6985da02a2f7c0
Aashish Adhikari <axisinfinity69@gmail.com>
2026-01-10 23:15:15 +0545

    Remove Firebase dependencies

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,17 +1,22 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBwW0lClhjMGX7wAxwJkelXL_rcgwcSSfE",
+  authDomain: "qr-app-e6b48.firebaseapp.com",
+  projectId: "qr-app-e6b48",
+  storageBucket: "qr-app-e6b48.firebasestorage.app",
+  messagingSenderId: "420148319216",
+  appId: "1:420148319216:web:6fd5ea29e07b2324428335",
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+// Initialize Auth with AsyncStorage persistence for React Native
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
 export const db = getFirestore(app);
